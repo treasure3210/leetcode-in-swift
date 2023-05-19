a8588f9539405f3010d61e0b432593179987c059
[treasure3210] <treasure3210@gmail.com>
2023-05-19 16:54:18 -0600

    Add thought process comments

--- LeetCode - Logger Rate Limiter.playground/Contents.swift
+++ LeetCode - Logger Rate Limiter.playground/Contents.swift
@@ -1,13 +1,18 @@
 class Logger {
 
+    // A dictionary (hash map) to store each message and its next eligible print timestamp
     var emptyHash: [String:Int] = [:]
+    
+    // Time constraint between two same messages: 10 seconds
     var timeOut = 10
 
     func shouldPrintMessage(_ timestamp: Int, _ message: String) -> Bool {
+        // If the message was printed recently, return false
         if let lastPrintedTimestamp = emptyHash[message], lastPrintedTimestamp > timestamp {
             return false
         }
 
+        // Update the next eligible print timestamp for the message
         emptyHash[message] = timestamp + timeOut
         return true
     }
