5baf36dcd37464f438483eee0c417c58068dd5fd
[treasure3210] <treasure3210@gmail.com>
2023-06-21 19:44:08 -0600

    test cast walkthrough

--- LeetCode - Linked List Cycle.playground/Contents.swift
+++ LeetCode - Linked List Cycle.playground/Contents.swift
@@ -18,6 +18,11 @@ class Solution {
         var fast = head
         
         // if in while loop, we find a matching case for slow and fast pointer, it means cycle exists
+        // test case: head = [3, 2, 0, -4], pos = 1
+        // first iteration: both slow and fast points 3 at the beginning, slow moves once so it's pointing 2, fast moves twice so it's pointing 0
+        // second iteration: slow points to 0, fast points to 2
+        // third iteration: slow points to -4, fast also points to -4
+        // they equal each other, iteration ends
         while fast != nil && fast!.next != nil {
             slow = slow!.next
             fast = fast!.next!.next
